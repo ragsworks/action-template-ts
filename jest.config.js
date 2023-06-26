b8fc83946619d52195064c1b87a7875687d042b6
Ragdata <ragdata@ragdata.dev>
2023-06-26 20:22:31 +1000

    chore(code): transfer code

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	clearMocks: true,
+	moduleFileExtensions: ['js', 'ts'],
+	testMatch: ['**/*.test.ts'],
+	testEnvironment: 'node',
+	transform: {
+		'^.+\\.ts$': 'ts-jest'
+	},
+	verbose: true
+}
